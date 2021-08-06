3211bf7219cd771890e14301551ceed0f5eb06ab
Alex Schmitt <alex.schmitt@outlook.com>
2021-08-06 00:37:49 -0300

    feature: course view and registration, personalized permissions
    Created personalized permissions and views to register courses
    and register students in the courses

---
+++ kanvas/permissions.py
@@ -0,0 +1,8 @@
+from rest_framework.permissions import BasePermission
+
+# --------------------------------------
+
+
+class IsInstructor(BasePermission):
+    def has_permission(self, request, view):
+        return request.user.is_superuser
